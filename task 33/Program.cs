a8cfb72f97d3893984fa48aafede08c86bbca3dd
Philo Tech <Philo Tech@DESKTOP-UOJAK0N>
2025-07-21 07:30:29 +0200

    Add project files.

---
+++ task 33/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace task_33
+{
+    class Book
+    {
+        string title;
+        string author;
+        string isbn;
+        bool availability;
+
+        public Book(string title, string author, string isbn, bool availability)
+        {
+            this.title = title;
+            this.author = author;
+            this.isbn = isbn;
+            this.availability = availability;
+        }
+    }
+    class Library
+    {
+        List<Book> books = new List<Book>();
+
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+        public void RemoveBook(Book book)
+        {
+            books.Remove(book);
+        }
+
+
+
+    }
+
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book book = new Book("sport", "ayad", "131245", true);
+            Library library = new Library();
+
+        }
+    }
+}
